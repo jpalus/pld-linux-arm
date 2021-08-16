baa859115116ab01745806f286ab8bae1bdbc41b
Jan Palus <jpalus@fastmail.com>
2021-08-16 13:23:49 +0200

    split logic into subcommands

--- create-chroot.sh
+++ create-chroot.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 log() {
+  if [ -z "$LOG_FILE" ]; then
+    LOG_FILE="$SCRIPT_DIR/$RELEASE_NAME-$ACTION.log"
+    : > "$LOG_FILE"
+    echo "Logging to $LOG_FILE"
+  fi
   echo "$@" >> "$LOG_FILE"
 }
 
@@ -27,8 +32,7 @@ run_log_priv() {
   run_log "$msg" $SUDO "$@"
 }
 
-:
-ARCH=${ARCH:-$(rpm -E '%{_host_cpu}')}
+ARCH=${ARCH:-$(rpm -E %{_host_cpu})}
 
 if [ $? -ne 0 ] || [ -z "$ARCH" ]; then
   error 'failed to determine arch'
@@ -39,46 +43,70 @@ if [ "$EUID" != "0" ]; then
   SUDO="sudo"
 fi
 
-
 SCRIPT_DIR=$(dirname $(readlink -f "$0"))
 TIMESTAMP=$(date +%Y%m%d)
 RELEASE_NAME="pld-linux-base-$ARCH-$TIMESTAMP"
-LOG_FILE="$SCRIPT_DIR/$RELEASE_NAME.log"
 DOCKER_REPO="jpalus/pld-linux-$ARCH"
 DOCKER_TAG="$DOCKER_REPO:$TIMESTAMP"
 DOCKER_TAG_LATEST="$DOCKER_REPO:latest"
-echo "Creating release $RELEASE_NAME"
 
-CHROOT_DIR="$(mktemp -t -d $RELEASE_NAME.XXXXXXXXXX)"
-if [ $? -ne 0 ]; then
-  error 'failed to create temporary chroot directory'
-fi
+create() {
+  echo "Creating release $RELEASE_NAME"
 
-: > "$LOG_FILE"
-echo "Logging to $LOG_FILE"
+  CHROOT_DIR="$(mktemp -t -d $RELEASE_NAME.XXXXXXXXXX)"
+  if [ $? -ne 0 ]; then
+    error 'failed to create temporary chroot directory'
+  fi
 
-run_log_priv "Setting up temporary chroot in $CHROOT_DIR" rpm --initdb --root "$CHROOT_DIR"
-run_log_priv "Installing packages from $SCRIPT_DIR/base.pkgs" poldek -iv --pset="$SCRIPT_DIR/base.pkgs" --root="$CHROOT_DIR" --noask --pmopt='--define=_tmppath\ /tmp'
-run_log "Preparing public key" gpg --output jpalus.asc --armor --export 'Jan Palus'
-run_log_priv "Importing public key" rpm --root="$CHROOT_DIR" --import jpalus.asc
-run_log_priv "Disabling default poldek repository configuration for $ARCH" sed -i -e "/^path.*=.*%{_prefix}\/PLD\/%{_arch}\/RPMS/ a auto = no\\nautoup = no" "$CHROOT_DIR/etc/poldek/repos.d/pld.conf"
-cat <<EOF | run_log_priv "Configuring custom $ARCH repository" sponge "$CHROOT_DIR/etc/poldek/repos.d/jpalus.conf"
+  run_log_priv "Setting up temporary chroot in $CHROOT_DIR" rpm --initdb --root "$CHROOT_DIR"
+  run_log_priv "Installing packages from $SCRIPT_DIR/base.pkgs" poldek -iv --pset="$SCRIPT_DIR/base.pkgs" --root="$CHROOT_DIR" --noask --pmopt='--define=_tmppath\ /tmp'
+  run_log "Preparing public key" gpg --output jpalus.asc --armor --export 'Jan Palus'
+  run_log_priv "Importing public key" rpm --root="$CHROOT_DIR" --import jpalus.asc
+  run_log_priv "Disabling default poldek repository configuration for $ARCH" sed -i -e "/^path.*=.*%{_prefix}\/PLD\/%{_arch}\/RPMS/ a auto = no\\nautoup = no" "$CHROOT_DIR/etc/poldek/repos.d/pld.conf"
+  cat <<EOF | run_log_priv "Configuring custom $ARCH repository" sponge "$CHROOT_DIR/etc/poldek/repos.d/jpalus.conf"
 [source]
 type = pndir
 name = jpalus
 path = http://jpalus.fastmail.com.user.fm/dists/th/PLD/$ARCH/RPMS/
 signed = yes
 EOF
-rpm --root="$CHROOT_DIR" -qa|sort > "$SCRIPT_DIR/$RELEASE_NAME.packages"
-run_log_priv "Creating archive $SCRIPT_DIR/$RELEASE_NAME.tar.xz" tar -Jcpf "$SCRIPT_DIR/$RELEASE_NAME.tar.xz" -C "$CHROOT_DIR" .
+  rpm --root="$CHROOT_DIR" -qa|sort > "$SCRIPT_DIR/$RELEASE_NAME.packages"
+  run_log_priv "Creating archive $SCRIPT_DIR/$RELEASE_NAME.tar.xz" tar -Jcpf "$SCRIPT_DIR/$RELEASE_NAME.tar.xz" -C "$CHROOT_DIR" .
 
-if [ "$EUID" != "0" ]; then
-  run_log_priv "Changing ownership of $RELEASE_NAME.tar.xz" chown $USER "$SCRIPT_DIR/$RELEASE_NAME.tar.xz"
-fi
+  if [ "$EUID" != "0" ]; then
+    run_log_priv "Changing ownership of $RELEASE_NAME.tar.xz" chown $USER "$SCRIPT_DIR/$RELEASE_NAME.tar.xz"
+  fi
 
-run_log 'Signing' gpg --sign --armor --detach-sig "$SCRIPT_DIR/$RELEASE_NAME.tar.xz"
-run_log_priv 'Cleaning up' rm -rf "$CHROOT_DIR"
-run_log "Importing docker image $DOCKER_TAG" podman import "$SCRIPT_DIR/$RELEASE_NAME.tar.xz" $DOCKER_TAG
-run_log "Tagging docker image $DOCKER_TAG as latest" podman tag $DOCKER_TAG $DOCKER_TAG_LATEST
-run_log "Pushing docker tag $DOCKER_TAG" podman push $DOCKER_TAG
-run_log "Pushing docker tag $DOCKER_TAG_LATEST" podman push $DOCKER_TAG_LATEST
+  run_log_priv 'Cleaning up' rm -rf "$CHROOT_DIR"
+}
+
+sign() {
+  run_log 'Signing' gpg --sign --armor --detach-sig "$SCRIPT_DIR/$RELEASE_NAME.tar.xz"
+}
+
+publish_dockerhub() {
+  run_log "Importing docker image $DOCKER_TAG" podman import "$SCRIPT_DIR/$RELEASE_NAME.tar.xz" $DOCKER_TAG
+  run_log "Tagging docker image $DOCKER_TAG as latest" podman tag $DOCKER_TAG $DOCKER_TAG_LATEST
+  run_log "Pushing docker tag $DOCKER_TAG" podman push $DOCKER_TAG
+  run_log "Pushing docker tag $DOCKER_TAG_LATEST" podman push $DOCKER_TAG_LATEST
+}
+
+case "$1" in
+  create|sign)
+    ACTION=$1
+    $1
+    ;;
+  publish)
+    case "$2" in
+      dockerhub)
+        ACTION=$1-$2
+        $1_$2
+        ;;
+      *)
+        error "Unknown publish target: $2"
+        ;;
+    esac
+    ;;
+  *)
+    error "Unknown action: $1"
+esac
