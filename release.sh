f35d92f305795955f28523c40c263e66254277be
Jan Palus <jpalus@fastmail.com>
2022-11-07 01:36:34 +0100

    use v2s2 manifest format when pushing docker tags

--- release.sh
+++ release.sh
@@ -168,8 +168,8 @@ publish_dockerhub() {
   esac
   run_log "Importing docker image $DOCKER_TAG" podman import --os linux --arch $IMAGE_ARCH ${IMAGE_VARIANT:+--variant $IMAGE_VARIANT} "$SCRIPT_DIR/$RELEASE_NAME.tar.xz" $DOCKER_REGISTRY/$DOCKER_TAG
   run_log "Tagging docker image $DOCKER_TAG as latest" podman tag $DOCKER_REGISTRY/$DOCKER_TAG $DOCKER_REGISTRY/$DOCKER_TAG_LATEST
-  run_log "Pushing docker tag $DOCKER_TAG" podman push $DOCKER_REGISTRY/$DOCKER_TAG
-  run_log "Pushing docker tag $DOCKER_TAG_LATEST" podman push $DOCKER_REGISTRY/$DOCKER_TAG_LATEST
+  run_log "Pushing docker tag $DOCKER_TAG" podman push -f v2s2 $DOCKER_REGISTRY/$DOCKER_TAG
+  run_log "Pushing docker tag $DOCKER_TAG_LATEST" podman push -f v2s2 $DOCKER_REGISTRY/$DOCKER_TAG_LATEST
 }
 
 image_unmount_fs() {
