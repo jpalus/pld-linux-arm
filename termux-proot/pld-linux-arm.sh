f4c483ff9714eed95396603ab23b5e81075a3b3a
Jan Palus <jpalus@fastmail.com>
2022-11-17 17:15:24 +0100

    add config for termux/proot-distro

---
+++ termux-proot/pld-linux-arm.sh
@@ -0,0 +1,6 @@
+DISTRO_NAME="PLD Linux Distribution"
+PLD_LINUX_ARM_VERSION=20221107
+TARBALL_URL['aarch64']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-$PLD_LINUX_ARM_VERSION/pld-linux-base-aarch64-$PLD_LINUX_ARM_VERSION.tar.xz"
+TARBALL_SHA256['aarch64']="3ffc119a32e281dbd9c4587578b094dc4be1b919a5321142aa72501677379dfc"
+TARBALL_URL['arm']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-$PLD_LINUX_ARM_VERSION/pld-linux-base-armv6hl-$PLD_LINUX_ARM_VERSION.tar.xz"
+TARBALL_SHA256['arm']="033a56555ae809ae73e5a3c47868a2a284765e6a4d75922c3793286d04a356ed"
