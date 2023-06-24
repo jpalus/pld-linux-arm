3f4e6a413ef0782f2647f340d5b893d241787ced
Jan Palus <jpalus@fastmail.com>
2023-06-24 19:06:28 +0200

    update information about image partitions instead of adding
    adding fails if partition information is already present but update
    adds them if they are not

--- release.sh
+++ release.sh
@@ -552,7 +552,7 @@ image_create() {
   image_dispatch image_prepare_file
   image_dispatch image_create_device
   image_dispatch image_create_partitions
-  run_log_priv "Probing for new partitions" partx -a $IMAGE_DEVICE
+  run_log_priv "Probing for new partitions" partx -u $IMAGE_DEVICE
   image_dispatch image_create_fs
   IMAGE_MOUNT_DIR=$(mktemp -d)
   image_dispatch image_mount_fs
