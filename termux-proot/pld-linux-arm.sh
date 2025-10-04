caecb86430bce74f37d2c10c126c5a4e55f24c69
Jan Palus <jpalus@fastmail.com>
2025-10-04 17:34:38 +0200

    update termux-proot config

--- termux-proot/pld-linux-arm.sh
+++ termux-proot/pld-linux-arm.sh
@@ -1,10 +1,10 @@
 DISTRO_NAME="PLD Linux Distribution"
-TARBALL_URL['aarch64']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-20250213/pld-linux-base-aarch64-20250213.tar.xz"
-TARBALL_SHA256['aarch64']="9c23f858b1a8a8d6c188b8651d2ff7c48d420dbfde39087cf237b43e6552413a"
+TARBALL_URL['aarch64']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-20251004/pld-linux-base-aarch64-20251004.tar.xz"
+TARBALL_SHA256['aarch64']="edbe1f9ac1677504d2c892be7cae6805d264916a408da52c80547dd5051e1ab1"
 if uname -m | grep -q armv7 && test -e /proc/cpuinfo && grep -q neon /proc/cpuinfo; then
-TARBALL_URL['arm']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-20250213/pld-linux-base-armv7hnl-20250213.tar.xz"
-TARBALL_SHA256['arm']="8c35aac4d863c8fe28f6f5d8baea0b674e195c27d40a275448700d6d8cd417d5"
+TARBALL_URL['arm']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-20251004/pld-linux-base-armv7hnl-20251004.tar.xz"
+TARBALL_SHA256['arm']="9cc616f4e8cd99f8f813f0fa9da65ffdf03ca3a64e826481ffad2768428c657e"
 else
-TARBALL_URL['arm']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-20250213/pld-linux-base-armv6hl-20250213.tar.xz"
-TARBALL_SHA256['arm']="20943aea057b4fa44b63a3aaf8c1a9913923000ee717a9b4401fe98a8766bccc"
+TARBALL_URL['arm']="https://github.com/jpalus/pld-linux-arm/releases/download/pld-linux-arm-20251004/pld-linux-base-armv6hl-20251004.tar.xz"
+TARBALL_SHA256['arm']="5ce6cb129e1c0c84be2bea7937208ef51878d963d41fce04833dd38020edc733"
 fi
